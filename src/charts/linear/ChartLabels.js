b562bad1f6c77b161d2ea7965e86d9cb93f0c365
Marcin Pawlicki <marcin.pawlicki@netguru.com>
2021-10-19 11:25:44 +0200

    style: remove redundant code

--- src/charts/linear/ChartLabels.js
+++ src/charts/linear/ChartLabels.js
@@ -11,17 +11,10 @@ const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
 
 function ChartLabelFactory(style) {
   return function ChartLabel({ format, ...props }) {
-    const { [style]: val = 0, providedData } = useContext(ChartContext);
+    const { [style]: val = 0 } = useContext(ChartContext);
     
     const formattedValue = useDerivedValue(() => {
-      if(val.value === '') {
-        const lastPosition = providedData?.points?.length - 1 || 0
-        const lastPoint = providedData?.points?.[`${lastPosition}`]
-        if(lastPoint) {
-          val.value = style == 'originalY' ? lastPoint.y : lastPoint.x
-        }
-      }
-    return format ? format(val.value) : val.value;
+      return format ? format(val.value) : val.value;
     }, []);
     const textProps = useAnimatedStyle(() => {
       return {
